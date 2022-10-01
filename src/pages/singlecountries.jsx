fecb09002e3f3b62bfddfb0ed62022660ac4db7a
zion livingstone <zionlivingstone@gmail.com>
2022-10-01 13:56:35 +0100

    added lang to country page

--- src/pages/singlecountries.jsx
+++ src/pages/singlecountries.jsx
@@ -4,19 +4,58 @@ import { useFetch } from '../context/Fetchprovider'
 export default function Singlecountries(params) {
     const data =useFetch()
     
-    const {name} =useParams()
-    
+    const {Country} =useParams()
 
     const singledata=data?.find((data)=>{
         
-        data.name===name
+       return data.name===Country
     })
-   
+    const lang=singledata.languages.map((lang)=>{
+        return lang.name
+    })
+    console.log(singledata);
+       
+
     return(
         <>
         <Navbar />
 
-        single country page {name}
+       <div className="single-country">
+        <button className="back">Back</button>
+
+        <div className="single-country-info">
+            <div className="single-country-flag">
+                <img src={singledata.flag} alt={`flag of ${singledata.name}`} />
+            </div>
+            <div className="single-country-info-text">
+            <h1 className="s-country-name">{singledata.name}</h1>
+                <div className="sc-column">
+                    
+                <div className="sc-column-1">
+                
+                <div className="s-country-N-name"><span>Native name:</span> {singledata.nativeName}</div>
+                <div className="s-country-population"><span>Population:</span> {singledata.population}</div>
+                <div className="s-country-region"><span>Region:</span> {singledata.region}</div>
+                <div className="s-country-subregion"><span>Sub-Region: </span>{singledata.subregion}</div>
+                <div className="s-country-capital"><span>Capital: </span>{singledata.capital}</div>
+                </div>
+                <div className="sc-column-2">
+                <div className="s-country-TLD"><span>Top-Level-Domain: </span>{singledata.topLevelDomain[0]}</div>
+                <div className="s-country-currency"><span>Currency: </span>{singledata.currencies[0].name}</div>
+                <div className="s-country-languages"><span>Languages: </span>{lang.join()}</div>
+                </div>
+                </div>
+                <div className="sc-border-country"><span>Border countries: </span>
+                    <div>
+                   { singledata.borders.map((borders)=>{
+                    return <div className="back">{borders}</div>
+                   })}</div>
+              </div>
+
+            </div>
+
+        </div>
+       </div>
         </>
     )
 }
